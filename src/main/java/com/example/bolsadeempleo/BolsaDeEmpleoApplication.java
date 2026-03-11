50b82bbf457b259a07a361c46bef23dbe33ff62a
Mariana Madrigal <mariana.madrigal.molina@est.una.ac.cr>
2026-03-11 13:25:30 -0600

    primer commit

---
+++ src/main/java/com/example/bolsadeempleo/BolsaDeEmpleoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bolsadeempleo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolsaDeEmpleoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BolsaDeEmpleoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bolsadeempleo/BolsaDeEmpleoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bolsadeempleo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolsaDeEmpleoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
